1747df80ac925912f5c1e3c9e0756471abcb00ef
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-05 12:45:03 +0100

    api

--- aws/lambdas/signup/index.js
+++ aws/lambdas/signup/index.js
@@ -1,8 +1,18 @@
-export const handler = async(event) => {
+export const handler = async (event) => {
     // TODO implement
+    var options = {
+        method: 'POST',
+
+        headers: { 'content-type': 'application/json' },
+        body: `{"client_id":"${process.env.client_id}","client_secret":"${process.env.client_secret}","audience":"${process.env.audience}' }`
+    };
+
+    await fetch(process.env.auth0_urltoken, options)
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from signUp!'),
     };
     return response;
 };
+
