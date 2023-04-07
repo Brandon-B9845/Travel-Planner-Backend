c676cba1e8d5ff79ce38fb4aa94bd540cf50eb27
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-07 20:19:50 +0100

    cleared up erros on our models index

--- models/index.ts
+++ models/index.ts
@@ -2,17 +2,17 @@
 
 import fs from 'fs';
 import path from 'path';
-import Sequelize from 'sequelize';
 import process from 'process';
+import { Sequelize } from 'sequelize-typescript';
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const db = {} as any;
 
 let sequelize;
 console.log('about to make sequelize connection...')
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config) ;
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
@@ -28,7 +28,7 @@ fs
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
     db[model.name] = model;
   });
 
