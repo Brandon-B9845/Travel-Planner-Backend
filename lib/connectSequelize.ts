b9b68a4658ccbfafc2a9f0878abb5ae1d772c176
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2023-04-10 07:46:43 -0500

    WIP

---
+++ lib/connectSequelize.ts
@@ -0,0 +1,52 @@
+import path, { join } from "path";
+import { Sequelize, SequelizeOptions } from "sequelize-typescript";
+import * as pg from 'pg';
+import { User } from "../models/Users";
+
+
+const basename = path.basename(__filename);
+
+export const connectSequelize = (database: string, username: string, password: string, host: string) => {
+  const db = {} as any;
+
+  const modelsArray = [User]
+
+  console.log('here is stange models array: ')
+  console.log(modelsArray)
+
+
+
+  const config: SequelizeOptions = {
+    dialectModule: pg,
+    dialect: 'postgres',
+    host: host,
+    models: modelsArray,
+    port: 5432,
+    define: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    },
+    hooks: {
+        afterConnect: (client: any, config) => {
+            console.log('db connected!')
+        },
+        afterDisconnect: () => {
+            console.log('disconnected db')
+        }
+    }
+  }
+
+  try{
+  const sequelize = new Sequelize(
+    database,
+    username,
+    password,
+    config
+  );
+  console.log('Successfully made db connection')
+  return sequelize
+  } catch (e) {
+    console.log('FAILED to make db connection...', e)
+    return null
+  }
+};
--- models/Users.ts
+++ models/Users.ts
@@ -1,18 +1,20 @@
-import { BOOLEAN, NUMBER, STRING } from 'sequelize';
-import { Table, Column, Model } from 'sequelize-typescript';
+import { BOOLEAN, DATE, INTEGER, STRING } from 'sequelize';
+import { Table, Column, Model, PrimaryKey } from 'sequelize-typescript';
 
 @Table({
     modelName: 'user',
     tableName: 'users'
 })
 export class User extends Model {
-    @Column(NUMBER)
+    @PrimaryKey
+    @Column(INTEGER)
     user_id: number;
+    
 
     @Column(STRING(50))
     email: string;
 
-    @Column(STRING(50))
+    @Column(STRING(255))
     password: string;
 
     @Column(BOOLEAN)
@@ -21,5 +23,10 @@ export class User extends Model {
     @Column(STRING(255))
     profile_image: string
 
-}
+    @Column(DATE)
+    createdAt?: Date
 
+    @Column(DATE)
+    updatedAt?: Date
+
+}
