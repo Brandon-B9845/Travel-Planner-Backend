ec582d64ac527e8c3db785af8eb4e7c1a17e77ff
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-10 16:14:34 +0100

    testing the sequlize migrations

--- migrations/20230410133451-create-users.js
+++ migrations/20230410133451-create-users.js
@@ -9,9 +9,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      user_id: {
-        type: Sequelize.NUMBER
-      },
       email: {
         type: Sequelize.STRING
       },
