61a8cad3abaee1ac1ab0fc62f9f93ef26d466732
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-13 18:43:44 +0100

    making models for our other 2 DB

---
+++ models/Saved_Attractions.ts
@@ -0,0 +1,59 @@
+
+import { BOOLEAN, DATE, INET, INTEGER, JSON, STRING, UUID, UUIDV4 } from 'sequelize';
+import { Table, Column, Model, PrimaryKey, CreatedAt, UpdatedAt, AllowNull, Default, ForeignKey } from 'sequelize-typescript';
+import { User } from './Users';
+
+// CREATE TABLE "public"."saved_attractions" (
+//     "name" varchar(255) NOT NULL,
+//     "address" varchar(255),
+//     "phone_number" varchar(20),
+//     "rating" numeric(4,2),
+//     "image_url" varchar(255),
+//     "description" varchar(255),
+//     "latitude" varchar(30),
+//     "longitude" varchar(30),
+//     "attraction_type" varchar(255),
+//     "review_count" int4,
+//     "user_id" int4
+@Table({
+    modelName: 'saved_attractions',
+    tableName: 'saved_attractions'
+})
+export class Saved_Attractions extends Model {
+    
+    
+    @Column(STRING(255))
+    name: string
+
+    @Column(STRING(255))
+    address: string
+
+    @Column(INTEGER)
+    rating: number
+
+    @Column(STRING(255))
+    image_url?: string
+
+    @Column(STRING(255))
+    description: string
+
+    @Column(STRING(50))
+    latitude: string
+
+    @Column(STRING)
+    longitude: string
+
+    @Column(STRING(255))
+    attractions_type: string
+
+    @Column(INTEGER)
+    review_count: number
+
+    @ForeignKey(()=> User )
+    @PrimaryKey
+    @Column(STRING)
+    email: string 
+
+}
+
+
---
+++ models/User_Trips.ts
@@ -0,0 +1,36 @@
+import { BOOLEAN, DATE, INTEGER, JSON, STRING, UUID, UUIDV4 } from 'sequelize';
+import { Table, Column, Model, PrimaryKey, CreatedAt, UpdatedAt, AllowNull, Default, ForeignKey } from 'sequelize-typescript';
+import { User } from './Users';
+
+// CREATE TABLE "public"."user_trips" (
+//     "name" varchar(255) NOT NULL,
+//     "image_url" varchar(255),
+//     "attractions" json,
+//     "created_at" timestamp,
+//     "user_id" int4 NOT NULL,
+//     CONSTRAINT "user_trips_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id")
+// );
+@Table({
+    modelName: 'user_trips',
+    tableName: 'user_trips'
+})
+export class User_trips extends Model {
+    
+    @Column(STRING(255))
+    name: string
+
+    @Column(STRING(255))
+    image_url?: string
+
+    @Column(JSON)
+    attractions: JSON
+
+    @Column(DATE)
+    created_at: Date
+
+    @ForeignKey(()=> User )
+    @PrimaryKey
+    @Column(STRING)
+    email: string 
+
+}
