4e237cc76dab0efdc0d0b81b316705be9eef6259
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-19 17:53:47 +0100

    saving progress

--- lambdas/signup/index.ts
+++ lambdas/signup/index.ts
@@ -1,22 +1,9 @@
-import { scryptSync, randomBytes, createHash } from "crypto"
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 import { connectSequelize } from "../../lib/connectSequelize"
 import { User } from "../../models/Users"
 import { bodyType } from "../../types/types"
 
-export function hash(input: string) {
-  return createHash("sha256").update(input).digest("hex")
-}
 
-export function getSafePassword(password: string) {
-  const hashResult = hash(password)
-
-  const salt = randomBytes(16).toString("hex")
-
-  const hashedPassword = scryptSync(hashResult, salt, 64)
-
-  return `${salt} : ${hashedPassword}`
-}
 
 export const handler = async (
   event: APIGatewayProxyEvent
@@ -34,12 +21,11 @@ export const handler = async (
   let body: bodyType = {}
 
   const connection = connectSequelize(database, username, password, host)
-  const userPassword = getSafePassword(data.password)
 
   try {
     await User.create({
       email: data.email,
-      password: userPassword,
+      id: data.id
     })
     body.success = true
     connection.close()
@@ -59,24 +45,7 @@ export const handler = async (
       body: JSON.stringify(body)
     }
   }
-  const options = {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: `{"client_id":"${client_id}","client_secret":"${client_secret}","audience":"${audience}" ,"grant_type":"client_credentials"}`,
-  }
-
-  try {
-
-    const auth0Response = await fetch(auth0_urlToken as string, options)
-    const jwt = await auth0Response.json()
-    const sendableJWT = jwt.access_token
-
-    body.jwt = sendableJWT
-  } catch (e) {
-    console.log("Failed to sign up: ", e)
-    body.error = 'Could not sign up'
-  }
-
+  
   return {
     statusCode: 200,
     body: JSON.stringify(body)
